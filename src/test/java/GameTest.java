d629755040d56b9bbd4cd9f905d97fe7db30abab
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-05-18 09:16:34 +0200

    Add tests for Joker win

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -259,7 +259,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("a player with a winning hand is declared the winner (No Joker). 1 straight if 6. 2 flushes of 4")
+    @DisplayName("a player with a winning hand is declared the winner (No Joker). 1 straight of 6. 2 flushes of 4")
     void test_15() {
         Player p = new Player("Hakan");
         Player p2 = new Player("Okan");
@@ -282,6 +282,68 @@ public class GameTest {
         tokens[12] = new Token(1, 10);
         tokens[13] = new Token(2, 10);
         tokens[14] = new Token(3, 10);
+
+        p.hand = tokens;
+        g.playerHandMap.put(p, tokens);
+
+        assertThat(g.currPlayerWon(t)).isTrue();
+    }
+
+    @Test
+    @DisplayName("a player with a winning hand is declared the winner (one Joker). 1 straight of 5. 2 flushes of 4")
+    void test_16() {
+        Player p = new Player("Hakan");
+        Player p2 = new Player("Okan");
+        Game g = new Game(new Player[]{p, p2});
+
+        Token[] tokens = new Token[15];
+        Token t = new Token(1, 1);
+        tokens[0] = t;
+        tokens[1] = new Token(1, 2);
+        tokens[2] = new Token(1, 3);
+        tokens[3] = new Token(1, 4);
+        tokens[4] = new Token(1, 5);
+        tokens[5] = new Token(1, 6);
+        tokens[6] = new Token(-1, -1);
+        tokens[7] = new Token(0, 9);
+        tokens[8] = new Token(1, 9);
+        tokens[9] = new Token(2, 9);
+        tokens[10] = new Token(3, 9);
+        tokens[11] = new Token(0, 10);
+        tokens[12] = new Token(1, 10);
+        tokens[13] = new Token(2, 10);
+        tokens[14] = new Token(3, 10);
+
+        p.hand = tokens;
+        g.playerHandMap.put(p, tokens);
+
+        assertThat(g.currPlayerWon(t)).isTrue();
+    }
+
+    @Test
+    @DisplayName("a player with a winning hand is declared the winner (two Joker). 1 straight of 8. 2 flushes of 2")
+    void test_17() {
+        Player p = new Player("Hakan");
+        Player p2 = new Player("Okan");
+        Game g = new Game(new Player[]{p, p2});
+
+        Token[] tokens = new Token[15];
+        Token t = new Token(1, 1);
+        tokens[0] = t;
+        tokens[1] = new Token(1, 2);
+        tokens[2] = new Token(1, 3);
+        tokens[3] = new Token(1, 4);
+        tokens[4] = new Token(1, 5);
+        tokens[5] = new Token(1, 6);
+        tokens[6] = new Token(-1, -1);
+        tokens[7] = new Token(0, 9);
+        tokens[8] = new Token(1, 9);
+        tokens[9] = new Token(0, 10);
+        tokens[10] = new Token(1, 10);
+        tokens[11] = new Token(-1, -1);
+        tokens[12] = new Token(1, 7);
+        tokens[13] = new Token(1, 8);
+        tokens[14] = new Token(1, 9);
 
         p.hand = tokens;
         g.playerHandMap.put(p, tokens);
